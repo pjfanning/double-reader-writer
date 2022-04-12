99fb15e51dc25efc1c7a6b76c80685125793cb55
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-12 15:01:41 +0200

    Update WriterTest.java

--- src/test/java/com/fasterxml/jackson/core/io/WriterTest.java
+++ src/test/java/com/fasterxml/jackson/core/io/WriterTest.java
@@ -1,7 +1,9 @@
 package com.fasterxml.jackson.core.io;
 
 import com.fasterxml.jackson.core.io.numberwriter.RyuDouble;
+import com.fasterxml.jackson.core.io.numberwriter.RyuFloat;
 import com.fasterxml.jackson.core.io.schubfach.DoubleToDecimal;
+import com.fasterxml.jackson.core.io.schubfach.FloatToDecimal;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
@@ -11,11 +13,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class WriterTest {
     private static final int LEN = 1000;
     private static final double[] DOUBLES = new double[LEN];
+    private static final float[] FLOATS = new float[LEN];
 
     static {
         Random rnd = new Random();
         for (int i = 0; i < LEN; i++) {
             DOUBLES[i] = rnd.nextDouble();
+            FLOATS[i] = rnd.nextFloat();
         }
     }
 
@@ -40,6 +44,30 @@ public class WriterTest {
         for (int i = 0; i < LEN; i++) {
             String jdk = Double.toString(DOUBLES[i]);
             assertEquals(DOUBLES[i], Double.parseDouble(jdk));
+        }
+    }
+
+    @Test
+    void verifySchubfachFloats() {
+        for (int i = 0; i < LEN; i++) {
+            String sf = FloatToDecimal.toString(FLOATS[i]);
+            assertEquals(FLOATS[i], Float.parseFloat(sf));
+        }
+    }
+
+    @Test
+    void verifyRyuFloats() {
+        for (int i = 0; i < LEN; i++) {
+            String ryu = RyuFloat.floatToString(FLOATS[i]);
+            assertEquals(FLOATS[i], Float.parseFloat(ryu));
+        }
+    }
+
+    @Test
+    void verifyJdkFloats() {
+        for (int i = 0; i < LEN; i++) {
+            String ryu = Float.toString(FLOATS[i]);
+            assertEquals(FLOATS[i], Float.parseFloat(ryu));
         }
     }
 
