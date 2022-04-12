292df99e19000225fa748d8d345e59d3b5818d9f
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-12 12:32:48 +0200

    extend reader tests

--- src/jmh/java/com/fasterxml/jackson/core/io/ReaderBenchmark.java
+++ src/jmh/java/com/fasterxml/jackson/core/io/ReaderBenchmark.java
@@ -1,33 +1,48 @@
 package com.fasterxml.jackson.core.io;
 
 import com.fasterxml.jackson.core.io.numberreader.FastDoubleParser;
-import com.fasterxml.jackson.core.io.numberwriter.RyuDouble;
 import org.openjdk.jmh.annotations.Benchmark;
 
 import java.util.Random;
 
 public class ReaderBenchmark extends BenchmarkLauncher {
     private static final int LEN = 1000;
-    private static final String[] STRINGS = new String[LEN];
+    private static final String[] DOUBLE_STRINGS = new String[LEN];
+    private static final String[] LONG_STRINGS = new String[LEN];
 
     static {
         Random rnd = new Random();
         for (int i = 0; i < LEN; i++) {
-            STRINGS[i] = Double.toString(rnd.nextDouble());
+            DOUBLE_STRINGS[i] = Double.toString(rnd.nextDouble());
+            LONG_STRINGS[i] = Long.toString(rnd.nextLong());
         }
     }
 
     @Benchmark
     public void jdkDoubleReader() {
         for (int i = 0; i < LEN; i++) {
-            Double.parseDouble(STRINGS[i]);
+            Double.parseDouble(DOUBLE_STRINGS[i]);
         }
     }
 
     @Benchmark
     public void fastDoubleReader() {
         for (int i = 0; i < LEN; i++) {
-            FastDoubleParser.parseDouble(STRINGS[i]);
+            FastDoubleParser.parseDouble(DOUBLE_STRINGS[i]);
+        }
+    }
+
+    @Benchmark
+    public void jdkLongReader() {
+        for (int i = 0; i < LEN; i++) {
+            Long.parseLong(LONG_STRINGS[i]);
+        }
+    }
+
+    @Benchmark
+    public void fastDoubleLongReader() {
+        for (int i = 0; i < LEN; i++) {
+            FastDoubleParser.parseDouble(LONG_STRINGS[i]);
         }
     }
 }
---
+++ src/test/java/com/fasterxml/jackson/core/io/ReaderTest.java
@@ -0,0 +1,43 @@
+package com.fasterxml.jackson.core.io;
+
+import com.fasterxml.jackson.core.io.numberreader.FastDoubleParser;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReaderTest {
+    private static final int LEN = 1000;
+    private static final String[] DOUBLE_STRINGS = new String[LEN];
+    private static final String[] LONG_STRINGS = new String[LEN];
+
+    static {
+        Random rnd = new Random();
+        for (int i = 0; i < LEN; i++) {
+            DOUBLE_STRINGS[i] = Double.toString(rnd.nextDouble());
+            LONG_STRINGS[i] = Long.toString(rnd.nextLong());
+        }
+    }
+
+    @Test
+    void verifyDoubles() {
+        for (int i = 0; i < LEN; i++) {
+            double fd = FastDoubleParser.parseDouble(DOUBLE_STRINGS[i]);
+            double jd = Double.parseDouble(DOUBLE_STRINGS[i]);
+            assertEquals(jd, fd);
+        }
+    }
+
+    @Disabled("this proves that parsing longs as doubles is a bad idea because of precision loss with large absolute values")
+    @Test
+    void verifyLongs() {
+        for (int i = 0; i < LEN; i++) {
+            long fl = (long)FastDoubleParser.parseDouble(LONG_STRINGS[i]);
+            long jl = Long.parseLong(LONG_STRINGS[i]);
+            assertEquals(jl, fl, "expected " + LONG_STRINGS[i]);
+        }
+    }
+
+}
