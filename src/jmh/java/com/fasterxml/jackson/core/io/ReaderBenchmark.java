3d52e0e3486d0c543a92fac29f0dbe9ee2494391
PJ Fanning <pjfanning@users.noreply.github.com>
2022-04-21 22:59:04 +0200

    Update ReaderBenchmark.java

--- src/jmh/java/com/fasterxml/jackson/core/io/ReaderBenchmark.java
+++ src/jmh/java/com/fasterxml/jackson/core/io/ReaderBenchmark.java
@@ -8,12 +8,14 @@ import java.util.Random;
 public class ReaderBenchmark extends BenchmarkLauncher {
     private static final int LEN = 1000;
     private static final String[] DOUBLE_STRINGS = new String[LEN];
+    private static final String[] FLOAT_STRINGS = new String[LEN];
     private static final String[] LONG_STRINGS = new String[LEN];
 
     static {
         Random rnd = new Random();
         for (int i = 0; i < LEN; i++) {
             DOUBLE_STRINGS[i] = Double.toString(rnd.nextDouble());
+            FLOAT_STRINGS[i] = Float.toString(rnd.nextFloat());
             LONG_STRINGS[i] = Long.toString(rnd.nextLong());
         }
     }
@@ -26,9 +28,23 @@ public class ReaderBenchmark extends BenchmarkLauncher {
     }
 
     @Benchmark
+    public void jdkFloatReader() {
+        for (int i = 0; i < LEN; i++) {
+            Float.parseFloat(FLOAT_STRINGS[i]);
+        }
+    }
+
+    @Benchmark
     public void fastDoubleReader() {
         for (int i = 0; i < LEN; i++) {
             FastDoubleParser.parseDouble(DOUBLE_STRINGS[i]);
+        }
+    }
+
+    @Benchmark
+    public void fastDoubleFloatReader() {
+        for (int i = 0; i < LEN; i++) {
+            float f = (float)FastDoubleParser.parseDouble(FLOAT_STRINGS[i]);
         }
     }
 
